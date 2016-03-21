ea6f364ea1b379d69935b2cf23dcf7e25b2516a5
Mathieu <mathieu.ancelin@gmail.com>
2016-03-21 16:31:56 +0100

    Add likes

---
+++ step-5/src/components/not-found.js
@@ -0,0 +1,9 @@
+import React, { PropTypes } from 'react';
+
+export const NotFound = React.createClass({
+  render() {
+    return (
+      <h2>Il semble que vous n'êtes pas au bon endroit !!!</h2>
+    );
+  }
+});
---
+++ step-5/webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+
+module.exports = {
+    output: {
+        path: './public/js/',
+        publicPath: '/js/',
+        filename: 'bundle.js'
+    },
+    entry: {
+        app: ['./src/app.js']
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            }
+        ]
+    }
+};
